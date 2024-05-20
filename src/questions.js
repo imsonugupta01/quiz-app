39978e3c26dcac073e65bbf4774484dac9131ef8
saloninarang27 <159940686+SaloniSonu@users.noreply.github.com>
2024-05-20 18:57:48 +0530

    initial committ

---
+++ src/questions.js
@@ -0,0 +1,31 @@
+// src/questions.js
+const questions = [
+    {
+      question: "What is the capital of France?",
+      options: ["Paris", "London", "Berlin", "Madrid"],
+      answer: "Paris",
+    },
+    {
+      question: "What is 2 + 2?",
+      options: ["3", "4", "5", "6"],
+      answer: "4",
+    },
+    {
+      question: "Which planet is known as the Red Planet?",
+      options: ["Earth", "Mars", "Jupiter", "Saturn"],
+      answer: "Mars",
+    },
+    {
+      question: "What is the largest ocean on Earth?",
+      options: ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
+      answer: "Pacific Ocean",
+    },
+    {
+      question: "Who wrote 'Hamlet'?",
+      options: ["William Shakespeare", "Charles Dickens", "Mark Twain", "J.K. Rowling"],
+      answer: "William Shakespeare",
+    },
+  ];
+  
+  export default questions;
+  
